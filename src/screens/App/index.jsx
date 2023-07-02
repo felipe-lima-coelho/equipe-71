9e6c9c4a00a2568236bb50f669fa889f53fa2183
Felipe Lima Coelho <felipeapoloatena@gmail.com>
2023-07-02 17:56:49 -0300

    (APP) Adicionado a lógica para desaparecer a mensagem do dia e também a iniciada a barra de progresso

--- src/screens/App/index.jsx
+++ src/screens/App/index.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './styles.module.css';
 import motivationalPhrases from '../../helpers/mock/motivationalPhrases';
+import Swal from 'sweetalert2';
 
 const msgMotivational = () => {
   const randomIndex = Math.floor(Math.random() * motivationalPhrases.length);
@@ -8,35 +9,77 @@ const msgMotivational = () => {
 };
 
 function App() {
+  const [isMsgAlreadySent, setIsMsgAlreadySent] = useState(false);
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    const isStorageMsgMotivational = localStorage.getItem('msgAlreadySent');
+
+    if (isStorageMsgMotivational === 'yes') {
+      setIsMsgAlreadySent(true);
+    }
+  }, []);
+
+  const increaseProgress = () => {
+    if (progress < 100) {
+      setProgress(prevProgress => prevProgress + 10);
+    }
+    if (progress >= 100) {
+      Swal.fire('Parabéns, você alcançou o seu objetivo nesse mês!')
+    }
+  };
+
+  const handleSaveResponseOfDay = () => {
+    localStorage.setItem('msgAlreadySent', 'yes');
+    setIsMsgAlreadySent(true);
+  };
+
   return (
     <div>
-      <section>
+      <main>
         <section>
-          <h1>Bom dia!</h1>
-          <p>{msgMotivational()}</p>
-        </section>
-
-        <section>
-          <strong>Como você está hoje?</strong>
-          <div>
-            <label htmlFor="triste">
-              Triste
-              <input type="radio" name="triste" id="triste" />
-            </label>
-            <label htmlFor="poucoTriste">
-              <input type="radio" name="poucoTriste" id="poucoTriste" />
-            </label>
-            <label htmlFor="poucoFeliz">
-              <input type="radio" name="poucoFeliz" id="poucoFeliz" />
-            </label>
-            <label htmlFor="feliz">
-              <input type="radio" name="feliz" id="feliz" />
-              Feliz
-            </label>
+          <div className={ styles.progressBarOuter }>
+            <div
+              className={ styles.progressBarInner } style={{ width: `${progress}%` }}
+            ></div>
           </div>
-          <button>Enviar</button>
         </section>
-      </section>
+      </main>
+
+      {
+        !isMsgAlreadySent && (
+          <section>
+            <section>
+              <h1>Bom dia!</h1>
+              <p>{msgMotivational()}</p>
+            </section>
+            <section>
+              <strong>Como você está hoje?</strong>
+              <div>
+                <label htmlFor="triste">
+                  Triste
+                  <input type="radio" name="triste" id="triste" />
+                </label>
+                <label htmlFor="poucoTriste">
+                  <input type="radio" name="poucoTriste" id="poucoTriste" />
+                </label>
+                <label htmlFor="poucoFeliz">
+                  <input type="radio" name="poucoFeliz" id="poucoFeliz" />
+                </label>
+                <label htmlFor="feliz">
+                  <input type="radio" name="feliz" id="feliz" />
+                  Feliz
+                </label>
+              </div>
+              <button
+                onClick={ handleSaveResponseOfDay }
+              >
+                Enviar
+              </button>
+            </section>
+          </section>
+        )
+      }
     </div>
   )
 }
