8d876718959c90ae37226cd7ad6c4c36f3d0e3b1
Felipe Lima Coelho <felipeapoloatena@gmail.com>
2023-07-02 23:08:59 -0300

    (FOOTER) Adicionado os ícones e estilizado

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,18 @@
 import { useContext } from "react";
 import headerContext from "../context/providers/headerContext";
 import styles from './styles/Footer.module.css';
+import { Gift, HandHeart, Target } from "@phosphor-icons/react";
 
 function Footer() {
   const { pageUrl, setPageUrl, points, setPoints } = useContext(headerContext);
 
   if (pageUrl !== '/' && pageUrl !== '/cadastro') {
     return (
-      <footer className={ styles.footerContent }>Footer</footer>
+      <footer className={ styles.footerContent }>
+        <Gift size={ 40 } />
+        <Target size={ 40 } />
+        <HandHeart size={ 40 } />
+      </footer>
     )
   }
 }
